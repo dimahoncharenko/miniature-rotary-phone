edd3a51e7d1bde4b91a762ef61650e9f7294a4ba
dimahoncharenko <d.goncharenko.man@gmail.com>
2021-04-05 13:27:14 +0300

    last commit

--- utils/connection.ts
+++ utils/connection.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connect = async () => {
     try {
-        await mongoose.connect("mongodb+srv://dima:raptor315@cluster0.3tt9x.mongodb.net/Articles?retryWrites=true&w=majority", {
+        await mongoose.connect(process.env.MONGO_URI!, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false
